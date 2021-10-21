4c5c937ea1af576c5f9087c65e0bb133ac2bd917
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-10-21 02:59:57 -0500

    Expres Auth0 mas toknes

--- server.js
+++ server.js
@@ -10,12 +10,30 @@ import rutasVenta from './views/ventas/rutas.js';
 import rutasUsuario from './views/usuarios/rutas.js';
 import rutasProducto from './views/productos/rutas.js';
 
+
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 dotenv.config({ path: './.env' });
+
 
 const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+  var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://firstsolution-proyventas.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticacion-ventas-mintic',
+  issuer: 'https://firstsolution-proyventas.us.auth0.com/',
+  algorithms: ['RS256']
+  });
+
+app.use(jwtCheck);
 app.use(rutasVenta);
 app.use(rutasUsuario);
 app.use(rutasProducto);
