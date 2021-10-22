f6775d56029bcfe110521719c998dbe0ce0325cb
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-10-22 00:44:27 -0500

    Expres Auth0 mas toknes

--- views/usuarios/rutas.js
+++ views/usuarios/rutas.js
@@ -5,6 +5,7 @@ import {
   editarUsuario,
   eliminarUsuario,
   consultarUsuario,
+  consultarOcrearUsuario
 } from '../../controllers/usuarios/controller.js';
 
 const rutasUsuario = Express.Router();
@@ -25,7 +26,10 @@ rutasUsuario.route('/usuarios').get((req, res) => {
 rutasUsuario.route('/usuarios').post((req, res) => {
   crearUsuario(req.body, genercCallback(res));
 });
-
+rutasUsuario.route('/usuarios/self').get((req, res) => {
+  console.log('alguien hizo get en la ruta /self');
+  consultarOcrearUsuario(req, genercCallback(res));
+});
 rutasUsuario.route('/usuarios/:id').get((req, res) => {
   console.log('alguien hizo get en la ruta /usuarios');
   consultarUsuario(req.params.id, genercCallback(res));
